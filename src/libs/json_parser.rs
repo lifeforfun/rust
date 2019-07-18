2a14be19f91d4dc0cbea1f3b62d68db7dc77f83b
zmm <a791446794@163.com>
2019-07-18 23:57:21 +0800

    json_parser

--- src/libs/json_parser.rs
+++ src/libs/json_parser.rs
@@ -1,12 +1,12 @@
 use std::collections::HashMap;
+use std::convert::TryFrom;
 use std::str::Chars;
 use std::str::FromStr;
-use std::convert::TryFrom;
 
 type F64 = f64;
 type I64 = i64;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, PartialOrd)]
 enum Number {
     I64(I64),
     F64(F64),
@@ -36,11 +36,11 @@ impl<'a> ParserIter<'a> {
         }
     }
 
-    fn get_str(&mut self, len: usize) -> Vec<char>{
+    fn get_str(&mut self, len: usize) -> Vec<char> {
         let mut i = 1;
-        let mut vc     = vec![];
+        let mut vc = vec![];
         loop {
-            if len<i {
+            if len < i {
                 break;
             }
             if let Some(c) = self.cursor {
@@ -63,57 +63,63 @@ impl<'a> ParserIter<'a> {
         }
     }
 
-    fn parse_literal(&mut self) -> Option<Result<Value, String>>{
-
+    fn parse_literal(&mut self) -> Option<Result<Value, String>> {
         let mut literal = None;
 
         if let Some(c) = self.cursor {
             match c {
                 't' => {
                     let s = self.get_str(4).into_iter().collect::<String>();
-                    if s=="true" {
+                    if s == "true" {
                         literal = Some(Ok(Value::Bool(true)));
                     } else {
-                        return Some(Err(format!("parse bool error: expect 'true' found {:?}", s)));
+                        return Some(Err(format!(
+                            "parse bool error: expect 'true' found {:?}",
+                            s
+                        )));
                     }
-                },
+                }
                 'f' => {
                     let s = self.get_str(5).into_iter().collect::<String>();
-                    if s=="false" {
+                    if s == "false" {
                         literal = Some(Ok(Value::Bool(false)));
                     } else {
-                        return Some(Err(format!("parse bool error: expect 'false' found {:?}", s)));
+                        return Some(Err(format!(
+                            "parse bool error: expect 'false' found {:?}",
+                            s
+                        )));
                     }
-                },
+                }
                 'n' => {
                     let s = self.get_str(4).into_iter().collect::<String>();
-                    if s=="null" {
+                    if s == "null" {
                         literal = Some(Ok(Value::Null));
                     } else {
                         return Some(Err(format!("parse error: expect 'null' found {}", s)));
                     }
-                },
-                _ => {},
+                }
+                _ => {}
             }
         }
-        // 确保返回之前向后移动游标
-        self.next();
+        if let Some(_) = literal {
+            // 确保返回之前向后移动游标,没匹配到有效字符不要移动
+            self.next();
+        }
         literal
     }
 
-    fn parse_number(&mut self) -> Option<Result<Value, String>>{
-
+    fn parse_number(&mut self) -> Option<Result<Value, String>> {
         let mut nv = vec![];
         while let Some(c) = self.cursor {
             match c {
-                '0'..='9'|'+'|'-'|'e'|'E'|'.' => {
+                '0'..='9' | '+' | '-' | 'e' | 'E' | '.' => {
                     nv.push(c);
                     self.next();
-                },
+                }
                 _ => break,
             }
         }
-        if nv.len()==0 {
+        if nv.len() == 0 {
             return None;
         }
         // 确保返回之前向后移动游标
@@ -121,73 +127,75 @@ impl<'a> ParserIter<'a> {
         let nstring = nv.into_iter().collect::<String>();
         let nstr = &nstring[..];
         if let Some(_) = nstr.find('.') {
-            Some(F64::from_str(nstr).map_err(|e| -> String {
-                format!("parse f64 error: {}", e)
-            }).map(|v| -> Value {
-                Value::Number(Number::F64(v))
-            }))
+            Some(
+                F64::from_str(nstr)
+                    .map_err(|e| -> String { format!("parse f64 error: {}", e) })
+                    .map(|v| -> Value { Value::Number(Number::F64(v)) }),
+            )
         } else {
-            Some(I64::from_str(nstr).map_err(|e| -> String {
-                format!("parse i64 error: {}", e)
-            }).map(|v| -> Value {
-                Value::Number(Number::I64(v))
-            }))
+            Some(
+                I64::from_str(nstr)
+                    .map_err(|e| -> String { format!("parse i64 error: {}", e) })
+                    .map(|v| -> Value { Value::Number(Number::I64(v)) }),
+            )
         }
     }
 
     fn parse_string(&mut self) -> Option<Result<Value, String>> {
-
         let mut name_start = false;
         let mut nv = vec![];
-        let c = self.cursor.unwrap();
-        if c!='"' {
+        let c = self.cursor?;
+        if c != '"' {
             return Some(Err(format!("unexpect character {}, expect '\"'", c)));
         }
         while let Some(c) = self.cursor {
             match c {
                 '\\' => {
-                    let next = self.next().unwrap();
+                    let next = self.next()?;
                     match next {
                         '"' => {
                             nv.push('"');
-                        },
+                        }
                         '\\' => {
                             nv.push('\\');
-                        },
+                        }
                         '/' => {
                             nv.push('/');
-                        },
+                        }
                         'b' => {
                             nv.push('\x08');
-                        },
+                        }
                         'f' => {
                             nv.push('\x0C');
-                        },
+                        }
                         'n' => {
                             nv.push('\n');
-                        },
+                        }
                         'r' => {
                             nv.push('\r');
-                        },
+                        }
                         't' => {
                             nv.push('\t');
-                        },
+                        }
                         'u' => {
                             let s = self.get_str(4).into_iter().collect::<String>();
                             if let Ok(i) = u32::from_str_radix(&s[..], 16) {
-                                if let Ok(ic)  = char::try_from(i) {
+                                if let Ok(ic) = char::try_from(i) {
                                     nv.push(ic);
                                 }
                             } else {
-                                return Some(Err(format!("escape unicode characters error : {}", s)));
+                                return Some(Err(format!(
+                                    "escape unicode characters error : {}",
+                                    s
+                                )));
                             }
-                        },
+                        }
                         _ => {
                             return Some(Err(format!("unexpected escaped character {}", next)));
-                        },
+                        }
                     };
                     self.next();
-                },
+                }
                 '"' => {
                     if name_start {
                         // 确保返回之前向后移动游标
@@ -195,19 +203,19 @@ impl<'a> ParserIter<'a> {
                         return Some(Ok(Value::String(nv.into_iter().collect::<String>())));
                     }
                     name_start = true;
-                },
+                }
                 other => {
                     nv.push(other);
-                },
+                }
             };
             self.next();
         }
         None
     }
 
-    fn parse_array(&mut self) -> Option<Result<Vec<Value>, String>>{
+    fn parse_array(&mut self) -> Option<Result<Value, String>> {
         let mut arr_start = false;
-        let mut nv: Vec<Value> = vec![];
+        let mut nv = vec![];
         while let Some(c) = self.cursor {
             match c {
                 ']' => {
@@ -216,8 +224,8 @@ impl<'a> ParserIter<'a> {
                     }
                     // 确保返回之前向后移动游标
                     self.next();
-                    return Some(Ok(nv));
-                },
+                    return Some(Ok(Value::Array(nv)));
+                }
                 ',' | '[' => {
                     if c == '[' {
                         arr_start = true;
@@ -228,29 +236,119 @@ impl<'a> ParserIter<'a> {
                     }
                     self.next();
                     self.trim_whitespaces();
-                    if let Some(Ok(v)) = self.parse_literal() {
+                    if let Some(Ok(v)) = self.parse_array() {
+                        nv.push(v);
+                    } else if let Some(Ok(v)) = self.parse_literal() {
                         nv.push(v);
                     } else if let Some(Ok(v)) = self.parse_number() {
                         nv.push(v);
                     } else if let Some(Ok(v)) = self.parse_string() {
                         nv.push(v);
+                    } else if let Some(Ok(v)) = self.parse_object() {
+                        nv.push(v);
                     } else {
-                        // TODO match object
-                        return Some(Err(format!("parse array error: {}", self.cursor.unwrap())));
+                        return Some(Err(format!(
+                            "parse array error: unexpected {}",
+                            self.cursor?
+                        )));
                     }
-                },
+                }
                 other => {
                     return Some(Err(format!("unexpected character '{}'", other)));
-                },
+                }
             }
             self.trim_whitespaces();
         }
         None
     }
 
+    fn parse_object(&mut self) -> Option<Result<Value, String>> {
+        let c = self.cursor?;
+        if c != '{' {
+            return Some(Err(format!(
+                "parse object failed: unexpected character {}",
+                c
+            )));
+        }
+
+        let mut ob_start = false;
+        let mut obj = HashMap::new();
+        while let Some(c) = self.cursor {
+            match c {
+                '{' => {
+                    ob_start = true;
+                    self.next();
+                    self.trim_whitespaces();
+                    if let Some(Ok(Value::String(name))) = self.parse_string() {
+                        let colon = self.cursor?;
+                        if colon != ':' {
+                            return Some(Err(format!("expect colon but found {}", colon)));
+                        }
+                        self.next();
+                        self.trim_whitespaces();
+                        if let Some(Ok(v)) = self.parse_object() {
+                            obj.insert(name, v);
+                        } else if let Some(Ok(v)) = self.parse_array() {
+                            obj.insert(name, v);
+                        } else if let Some(Ok(v)) = self.parse_literal() {
+                            obj.insert(name, v);
+                        } else if let Some(Ok(v)) = self.parse_string() {
+                            obj.insert(name, v);
+                        } else if let Some(Ok(v)) = self.parse_number() {
+                            obj.insert(name, v);
+                        } else {
+                            return Some(Err(format!(
+                                "parse object error, unexpected {}",
+                                self.cursor?
+                            )));
+                        }
+                    }
+                }
+                '}' => {
+                    if !ob_start {
+                        return Some(Err(format!("unexpected object close brach")));
+                    }
+                    self.next();
+                    return Some(Ok(Value::Object(obj)));
+                }
+                ',' => {
+                    self.next();
+                    self.trim_whitespaces();
+                    if let Some(Ok(Value::String(name))) = self.parse_string() {
+                        let colon = self.cursor?;
+                        if colon != ':' {
+                            return Some(Err(format!("expect colon but found {}", colon)));
+                        }
+                        self.next();
+                        self.trim_whitespaces();
+                        if let Some(Ok(v)) = self.parse_object() {
+                            obj.insert(name, v);
+                        } else if let Some(Ok(v)) = self.parse_array() {
+                            obj.insert(name, v);
+                        } else if let Some(Ok(v)) = self.parse_literal() {
+                            obj.insert(name, v);
+                        } else if let Some(Ok(v)) = self.parse_string() {
+                            obj.insert(name, v);
+                        } else if let Some(Ok(v)) = self.parse_number() {
+                            obj.insert(name, v);
+                        } else {
+                            return Some(Err(format!(
+                                "parse object error, unexpected {}",
+                                self.cursor?
+                            )));
+                        }
+                    }
+                }
+                c => return Some(Err(format!("unexpected character {}", c))),
+            }
+            self.next();
+            self.trim_whitespaces();
+        }
+        None
+    }
 }
 
-impl <'a>Iterator for ParserIter<'a> {
+impl<'a> Iterator for ParserIter<'a> {
     type Item = char;
     fn next(&mut self) -> Option<Self::Item> {
         self.cursor = self.s.next();
@@ -258,18 +356,21 @@ impl <'a>Iterator for ParserIter<'a> {
     }
 }
 
-pub fn test()
-{
+pub fn test() {
     let data = r#"
-        [
+        {
+        "test": [
         "test中国\nfdsfs",
-        "test"
+        "方法",
+        100
         ]
-    "#.to_string();
+        }
+    "#
+    .to_string();
     {
         let mut chars = data.chars();
         let mut pit = ParserIter::new(&mut chars);
         pit.trim_whitespaces();
-        println!("{:?}", pit.parse_array());
+        println!("{:?}", pit.parse_object());
     }
 }
