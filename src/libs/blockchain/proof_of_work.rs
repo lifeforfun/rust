b9638fd8587f7528215f19abfd62a8c981654ad1
zmm <a791446794@163.com>
2019-07-31 23:07:58 +0800

    blockchain

--- src/libs/blockchain/proof_of_work.rs
+++ src/libs/blockchain/proof_of_work.rs
@@ -1,10 +1,28 @@
+extern crate crypto;
+
 use crate::libs::blockchain::block_chain::*;
+use crypto::digest::Digest;
+use crypto::sha3::Sha3;
 
 const TARGET_BIT: u64 = 20;
 
 struct ProofOfWork {
     pub block: Block,
     pub target: u64,
+}
+
+fn copy_from_slice(&s: &[u8]) -> [u8; 8]
+{
+    let mut arr = [0; 8];
+    let counter = 0;
+    for i in s.iter() {
+        if counter>=arr.len() {
+            break;
+        }
+        arr[counter] = i.clone();
+        counter += 1;
+    }
+    arr
 }
 
 impl ProofOfWork {
@@ -24,5 +42,29 @@ impl ProofOfWork {
         v.append(&mut format!("{:x}", TARGET_BIT).as_bytes().to_vec());
         v.append(&mut format!("{:x}", nonce).as_bytes().to_vec());
         v
+    }
+
+    fn run(&mut self) -> (i32, Vec<u8>) {
+        let hash_int;
+        let hash;
+        let nonce = 0;
+        println!("Mining the block containing \"{:?}\"", self.block.data);
+
+        loop {
+            if nonce>=i32::max_value() {
+                break;
+            }
+            let data = self.prepare_data(nonce);
+            let mut hasher = Sha3::sha3_256();
+            hasher.input(&data[..]);
+            hash_int = u64::from_be_bytes(copy_from_slice(hasher.result_str().as_bytes()));
+
+            if hash_int<self.target {
+                break;
+            } else {
+                nonce += 1;
+            }
+        }
+        (nonce, hash)
     }
 }
