67690806b3de7e97116ed37cb01f1df07ece8c40
zhangmingming <zhangmingming@doweidu.com>
2020-03-07 17:31:57 +0800

    ffi

--- build.rs
+++ build.rs
@@ -1,22 +1,25 @@
-#[cfg(target_os = "windows")]
+
 fn main() {
+
+    #[cfg(target_os = "windows")]
+        {
 //    if let Ok(vulkan_sdk) = std::env::var("VULKAN_SDK") {
 //        println!("cargo:rustc-link-lib=static=vulkan");
 //        println!("cargo:rustc-link-search=native={}/Lib", vulkan_sdk);
 //    }
-}
+        }
 
-#[cfg(target_os = "linux")]
-fn main() {
-//    println!("cargo:rustc-link-lib=dylib=vulkan");
-}
-
-#[cfg(target_os="macos")]
-fn main () {
+    #[cfg(target_os = "linux")]
+        {
+            //    println!("cargo:rustc-link-lib=dylib=vulkan");
+        }
+    #[cfg(target_os="macos")]
+        {
 //    if let Ok(vulkan_sdk) = std::env::var("VULKAN_SDK") {
 //        println!("cargo:rustc-link-lib=dylib=vulkan");
 //        println!("cargo:rustc-link-search=native={}/lib", vulkan_sdk);
 //    } else {
 //        panic!("unspecified environment `VULKAN_SDK`");
 //    }
+        }
 }
